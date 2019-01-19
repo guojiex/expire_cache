c292ffbd16a5aa06ebe61d36e225854adc232502
guojiex <1036479561@qq.com>
2019-01-19 11:00:42 -0500

    Add test and implement. Add travis config.

--- lib/flutter_expire_cache.dart
+++ lib/flutter_expire_cache.dart
@@ -1,21 +1,49 @@
 library flutter_expire_cache;
 
 import 'dart:core';
+import 'dart:async';
 import 'package:clock/clock.dart';
 
 class _CacheEntry<V> {
   final _cacheObject;
-  _CacheEntry(this._cacheObject);
+  final DateTime _createTime;
+  _CacheEntry(this._cacheObject, this._createTime);
 }
 
 /// A cache. Its entries will expire after a given time period.
 class ExpireCache<K, V> {
   final Duration expireDuration;
+  final Duration gcDuration;
+  final Clock clock;
+  Timer _expireTimer;
   Map<K, _CacheEntry<V>> _cache = Map<K, _CacheEntry<V>>();
 
-  ExpireCache({this.expireDuration = const Duration(seconds: 60)});
+  ExpireCache(
+      {this.clock = const Clock(),
+      this.expireDuration = const Duration(seconds: 60),
+      this.gcDuration = const Duration(seconds: 60)});
 
-  V get(K key){
-    return _cache[key]._cacheObject;
+  Future<Null> set(K key, V value) async {
+    _cache[key] = _CacheEntry(value, clock.now());
+    _expireTimer ??= Timer(gcDuration, _expireOutdatedEntries);
+  }
+
+  Future<Null> invalidate(K key) async {
+    _cache.remove(key);
+  }
+
+  Future<Null> _expireOutdatedEntries() async {
+    _cache.keys
+        .where((key) =>
+            clock.now().difference(_cache[key]._createTime) > expireDuration)
+        .toList()
+        .forEach(_cache.remove);
+  }
+
+  Future<V> get(K key) async {
+    if (_cache.containsKey(key)) {
+      return _cache[key]._cacheObject;
+    }
+    return null;
   }
 }
--- test/flutter_expire_cache_test.dart
+++ test/flutter_expire_cache_test.dart
@@ -1,9 +1,48 @@
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:fake_async/fake_async.dart';
 import 'package:flutter_expire_cache/flutter_expire_cache.dart';
 
 void main() {
-  test('adds one to input values', () {
-    var cache = ExpireCache<String,String>();
+  group("Basic Cache Test", () {
+    ExpireCache<String, String> cache;
+    setUp(() async {
+      cache = ExpireCache<String, String>();
+    });
+    test('test cache set get', () async {
+      cache.set('key', 'value');
+      expect(await cache.get('key'), 'value');
+      expect(await cache.get('not exist'), null);
+    });
+    test('test cache invalidate', () async {
+      cache.set('key', 'value');
+      cache.invalidate('key');
+      expect(await cache.get('key'), null);
+    });
+  });
+  group("Test Expire", () {
+    test('test cache entry gets expired', () {
+      new FakeAsync().run((async) {
+        final expireDuration = Duration(seconds: 20);
+        ExpireCache<String, String> cache = ExpireCache<String, String>(
+            expireDuration: expireDuration, gcDuration: expireDuration);
+        cache.set('key', 'value');
+        cache.get('key').then((String value)=>expect(value, 'value'));
+        async.elapse(expireDuration);
+        cache.get('key').then((String value)=>expect(value, null));
+      });
+    });
+    test('test gc duration', () {
+      new FakeAsync().run((async) {
+        final expireDuration = Duration(seconds: 20);
+        final gcDuration = Duration(seconds: 60);
+        ExpireCache<String, String> cache = ExpireCache<String, String>(
+            expireDuration: expireDuration, gcDuration: gcDuration);
+        cache.set('key', 'value');
+        async.elapse(expireDuration);
+        cache.get('key').then((String value)=>expect(value, 'value'));
+        async.elapse(gcDuration - expireDuration);
+        cache.get('key').then((String value)=>expect(value, null));
+      });
+    });
   });
 }
