466ec9c1c17a416c6d97b95863bed277289d14d0
guojiex <1036479561@qq.com>
2019-01-18 22:14:45 -0500

    Draft commit.

--- lib/flutter_expire_cache.dart
+++ lib/flutter_expire_cache.dart
@@ -1,7 +1,21 @@
 library flutter_expire_cache;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:core';
+import 'package:clock/clock.dart';
+
+class _CacheEntry<V> {
+  final _cacheObject;
+  _CacheEntry(this._cacheObject);
+}
+
+/// A cache. Its entries will expire after a given time period.
+class ExpireCache<K, V> {
+  final Duration expireDuration;
+  Map<K, _CacheEntry<V>> _cache = Map<K, _CacheEntry<V>>();
+
+  ExpireCache({this.expireDuration = const Duration(seconds: 60)});
+
+  V get(K key){
+    return _cache[key]._cacheObject;
+  }
 }
